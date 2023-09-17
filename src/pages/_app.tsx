01a1b670d6c7c9372b89a54fe6f25a2c028ab01f
Jan Henning <triton539@outlook.com>
2023-09-17 21:54:55 +0200

    use script tag rather than next-plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import PlausibleProvider from 'next-plausible'
 
 import '../styles/globals.css'
 
@@ -11,17 +10,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-F6VJZ5H2G5'></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: 'window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'G-F6VJZ5H2G5\');'
-          }}
-        ></script>
+        <script defer data-domain='definecultured.com' src='https://analytics.definecultured.com/js/script.js'></script>
       </Head>
-      <PlausibleProvider domain="definecultured.com">
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </PlausibleProvider>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
     </>
   )
 }
