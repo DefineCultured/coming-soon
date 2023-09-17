34a8b03726803b7ad2f168356e92bf28973b388e
Jan Henning <triton539@outlook.com>
2023-09-17 19:17:46 +0200

    add next-plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { DefaultSeo } from 'next-seo'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import PlausibleProvider from 'next-plausible'
 
 import '../styles/globals.css'
 
@@ -13,12 +14,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <script async src='https://www.googletagmanager.com/gtag/js?id=G-F6VJZ5H2G5'></script>
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-F6VJZ5H2G5');`
+            __html: 'window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'G-F6VJZ5H2G5\');'
           }}
         ></script>
       </Head>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <PlausibleProvider domain="definecultured.com">
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </PlausibleProvider>
     </>
   )
 }
